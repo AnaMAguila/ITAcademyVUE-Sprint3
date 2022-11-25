18da5e415c7f016c98238789e2dffa46c0c3ac55
Ana María Águila <ana_mari_aguila@hotmail.com>
2022-11-25 13:42:52 +0100

    Validar formulario con JavaScript

--- exercici7/js/checkout.js
+++ exercici7/js/checkout.js
@@ -39,16 +39,20 @@ window.addEventListener('load', ()=> {
 		if(fName.value.length < 3 || !validLetters.test(fName.value)){
 			error++;
 			// errorName.style.display = "block";
+			fName.classList.remove('is-valid');
 			fName.classList.add('is-invalid');
 		}else{
 			fName.classList.remove('is-invalid');
+			fName.classList.add('is-valid');
 		}
 
 		if(fLastN.value.length < 3 || !validLetters.test(fLastN.value)){
 			error++;
+			fLastN.classList.remove('is-valid');
 			fLastN.classList.add('is-invalid');
 		}else{
 			fLastN.classList.remove('is-invalid');
+			fLastN.classList.add('is-valid');
 		}
 
 		//Validate email
@@ -56,17 +60,21 @@ window.addEventListener('load', ()=> {
 				
 		if(fEmail.value.length < 3 || !validEmail.test(fEmail.value)){
 			error++;
+			fEmail.classList.remove('is-valid');
 			fEmail.classList.add('is-invalid');
 		}else{
 			fEmail.classList.remove('is-invalid');
+			fEmail.classList.add('is-valid');
 		}
 
 		//Validate address
 		if(fAddress.value.length < 3){
 			error++;
+			fAddress.classList.remove('is-valid');
 			fAddress.classList.add('is-invalid');
 		}else{
 			fAddress.classList.remove('is-invalid');
+			fAddress.classList.add('is-valid');
 		}
 
 		//Validate password
@@ -74,9 +82,11 @@ window.addEventListener('load', ()=> {
 
 		if(fPassword.value.length < 3 || !validPassword.test(fPassword.value)){
 			error++;
+			fPassword.classList.remove('is-valid');
 			fPassword.classList.add('is-invalid');
 		}else{
 			fPassword.classList.remove('is-invalid');
+			fPassword.classList.add('is-valid');
 		}
 
 		//Validate phone
@@ -84,9 +94,11 @@ window.addEventListener('load', ()=> {
 
 		if(fPhone.value.length < 3 || !validPhone.test(fPhone.value)){
 			error++;
+			fPhone.classList.remove('is-valid');
 			fPhone.classList.add('is-invalid');
 		}else{
 			fPhone.classList.remove('is-invalid');
+			fPhone.classList.add('is-valid');
 		}
 
 		//Checkout inform is all is valid
